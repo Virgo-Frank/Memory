c9dbd02f09c543e07ddfccbee5408acd769dd1c2
Virgo Frank <virgofrank2@gmail.com>
2022-07-19 22:28:59 +0300

    add promt for string length

--- Memory/Memory.cpp
+++ Memory/Memory.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <vector>
 using namespace std;
 
 using namespace std::this_thread;
@@ -9,22 +10,29 @@ using namespace std::chrono;
 
 void printStr() {
 	srand(time(0));
-	string str = "";
 	int rnd;
-	for (int i = 0; i < 4; i++)
+	int strLength;
+	cout << "enter string length\n";
+	cin >> strLength;
+	vector<int> strList;
+	system("cls");
+	for (int i = 0; i < strLength; i++)
 	{
 		rnd = rand() % 10;
 		std::cout << rnd;
 		std::cout << "\n";
-		str += to_string(rnd);
+		strList.push_back(rnd);
 
 		sleep_for(seconds(1));
 		system("cls");
 	}
-	std::cout << "input to reveal string";
+	std::cout << "input to reveal string\n";
 	cin.get();
-	cout << str;
-
+	cin.get();
+	for (int i = 0; i < strList.size(); i++)
+	{
+		cout << strList[i];
+	}
 }
 
 
