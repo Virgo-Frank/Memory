72749c936a16725ad60605d321d8beaf8f733398
Virgo Frank <virgofrank2@gmail.com>
2022-08-01 21:55:18 +0300

    Add a contiuous mode and a function takes the string you are trying to remember and checks if it is correct

--- Memory/Memory.cpp
+++ Memory/Memory.cpp
@@ -8,6 +8,8 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
+bool checkStr(vector<int> strList);
+
 void printStr() {
 	srand(time(0));
 	int rnd;
@@ -35,13 +37,76 @@ void printStr() {
 	}
 }
 
+void continuous() {
+	srand(time(0));
+	int rnd;
+	int strLength;
+	cout << "enter string length\n";
+	cin >> strLength;
+	vector<int> strList;
+	while (true) {
+		system("cls");
+		for (int i = 0; i < strLength; i++)
+		{
+			rnd = rand() % 10;
+			std::cout << rnd;
+			std::cout << "\n";
+			strList.push_back(rnd);
+
+			sleep_for(seconds(1));
+			system("cls");
+		}
+		std::cout << "input to reveal string\n";
+		cin.get();
+		cin.get();
+		for (int i = 0; i < strList.size(); i++)
+		{
+			cout << strList[i];
+		}
+		cin.get();
+		checkStr(strList);
+		strList.clear();
+	}
+	
+}
+
+bool checkStr(vector<int> strList) {
+	
+	system("cls");
+	std::cout << "Enter string:\n";
+	string input;
+	input = cin.get();
+	system("cls");
+	if (input.size() == strList.size())
+	{
+		for (int i = 0; i < strList.size(); i++)
+		{
+			if (input[i] != strList[i]) {
+				std::cout << "Incorrect string";
+				cin.get();
+				return false;
+			}
+		}
+		return true;
+	}
+	std::cout << "Incorrect string";
+	cin.get();
+	return false;
+
+}
+
 
 int main()
 {
+	string promt;
+	cout << "continuous? (y/n)\n";
+	promt = cin.get();
+	if (promt == "y")
+		continuous();
+
 	printStr();
 	bool check = true;
 	while (check) {
-		string promt;
 		cin.get();
 		system("cls");
 		cout << "\nRepeat? y/n\n";
